685dd5d2aeadb0c61c16652e4b3a2df99eb534fa
kevinkoscica <kevinkoscica@gmail.com>
2025-01-21 12:52:32 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Kevin Koscica on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
